599abf82db42f4c79081a50991f4471377679bad
rand777 <rand777@pguide.studio>
2025-08-09 13:56:42 +0800

    docs(notice/migrate): add notification of site domain migration

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -2,49 +2,6 @@ import {defineThemeConfig} from 'vuepress-theme-plume'
 import {navbar} from './navbar'
 import notes from './notes'
 import {SpeedInsights} from '@vercel/speed-insights/vue'
-// import fetch from 'node-fetch';
-// import fs from 'fs';
-
-
-// async function fetchCommits() {
-//   const res = await fetch('https://api.github.com/repos/Lyrlark/PGuide-Docs/commits?sha=master');
-//   const commits = await res.json();
-//   return commits.map(c => `commit id:"${c.sha.slice(0,7)}" tag:"${c.commit.message.split('\n')[0]}"`).join('\n');
-// }
-//
-// async function updateGitGraph() {
-//   const graph = await fetchCommits();
-//   let md = fs.readFileSync('docs/notes/begin.md', 'utf-8');
-//   md = md.replace(/```git-graph[\s\S]*?```/, '```git-graph\n' + graph + '\n```');
-//   fs.writeFileSync('docs/notes/begin.md', md);
-// }
-//
-// updateGitGraph();
-
-// 首页异步检查各服务在线状态
-// async function checkStatus() {
-//     try {
-//         const response = await fetch('https://status.cqmu.online/api/badge/14/status');
-//         if (response.ok) {
-//             const svgText = await response.text();
-//             if (svgText.includes('Status: Up')) {
-//                 return '所有服务均在线';
-//             }
-//         }
-//         return '部分服务离线（此处待维护）';
-//     } catch (error) {
-//         console.error('Error fetching status:', error);
-//         return '服务离线';
-//     }
-// }
-//
-// // 返回信息
-//
-// async function getFooterMessage() {
-//     const statusMessage = await checkStatus();
-//     return `${statusMessage} <a href="https://status.cqmu.online/" target="_blank"><img src="https://status.cqmu.online/api/badge/14/status?style=plastic" alt="Status Badge" style="vertical-align: middle; display: inline-block;"></a>`;
-// }
-
 
 /**
  * @see https://theme-plume.vuejs.press/config/basic/
@@ -80,19 +37,10 @@ export default defineThemeConfig({
     aside: true, // 页内侧边栏， 默认显示在右侧
     outline: [2, 6], // 页内大纲， 默认显示 h2, h3
 
-    /**
-     * 文章版权信息
-     * @see https://theme-plume.vuejs.press/guide/features/copyright/
-     */
-    // copyright: true,
-
-    // prevPage: true,   // 是否启用上一页链接
-    // nextPage: true,   // 是否启用下一页链接
-    // createTime: true, // 是否显示文章创建时间
-
     /* 站点页脚，message显示在线情况 */
     footer: {
-        message: '<a href="https://docs.pguide.cloud" target="_blank" style="transition: all 0.3s ease; display: inline-block; background: linear-gradient(270deg, #ff6b6b, #4ecdc4, #ff6b6b); background-size: 200% 100%; animation: moveGradient 3s infinite linear; -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-weight: bold; position: relative;" onmouseover="this.style.transform=\'scale(1.1)\'; this.style.animationDuration=\'1s\'" onmouseout="this.style.transform=\'scale(1)\'; this.style.animationDuration=\'3s\'">国内镜像站点</a> | <a href="https://docs.pguide.studio" target="_blank" style="transition: all 0.3s ease; display: inline-block; background: linear-gradient(270deg, #a8e6cf, #3498db, #a8e6cf); background-size: 200% 100%; animation: moveGradient 3s infinite linear; -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-weight: bold; position: relative;" onmouseover="this.style.transform=\'scale(1.1)\'; this.style.animationDuration=\'1s\'" onmouseout="this.style.transform=\'scale(1)\'; this.style.animationDuration=\'3s\'">国际主站点</a> | <a href="https://ecosystem.pguide.studio" target="_blank" style="transition: all 0.3s ease; display: inline-block; background: linear-gradient(270deg, #dcd6f7, #424874, #dcd6f7); background-size: 200% 100%; animation: moveGradient 3s infinite linear; -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-weight: bold; position: relative;" onmouseover="this.style.transform=\'scale(1.1)\'; this.style.animationDuration=\'1s\'" onmouseout="this.style.transform=\'scale(1)\'; this.style.animationDuration=\'3s\'">项导生态</a><style>@keyframes moveGradient { 0% { background-position: 200% 0; } 100% { background-position: -200% 0; }}</style>',
+        // message: '<a href="https://docs.pguide.cloud" target="_blank" style="transition: all 0.3s ease; display: inline-block; background: linear-gradient(270deg, #ff6b6b, #4ecdc4, #ff6b6b); background-size: 200% 100%; animation: moveGradient 3s infinite linear; -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-weight: bold; position: relative;" onmouseover="this.style.transform=\'scale(1.1)\'; this.style.animationDuration=\'1s\'" onmouseout="this.style.transform=\'scale(1)\'; this.style.animationDuration=\'3s\'">国内镜像站点</a> | <a href="https://docs.pguide.studio" target="_blank" style="transition: all 0.3s ease; display: inline-block; background: linear-gradient(270deg, #a8e6cf, #3498db, #a8e6cf); background-size: 200% 100%; animation: moveGradient 3s infinite linear; -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-weight: bold; position: relative;" onmouseover="this.style.transform=\'scale(1.1)\'; this.style.animationDuration=\'1s\'" onmouseout="this.style.transform=\'scale(1)\'; this.style.animationDuration=\'3s\'">国际主站点</a> | <a href="https://ecosystem.pguide.studio" target="_blank" style="transition: all 0.3s ease; display: inline-block; background: linear-gradient(270deg, #dcd6f7, #424874, #dcd6f7); background-size: 200% 100%; animation: moveGradient 3s infinite linear; -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-weight: bold; position: relative;" onmouseover="this.style.transform=\'scale(1.1)\'; this.style.animationDuration=\'1s\'" onmouseout="this.style.transform=\'scale(1)\'; this.style.animationDuration=\'3s\'">项导生态</a><style>@keyframes moveGradient { 0% { background-position: 200% 0; } 100% { background-position: -200% 0; }}</style>',
+        message: '',
         copyright: 'PGuide Studio © 2023-2025',
     },
     notFound: {
@@ -126,16 +74,15 @@ export default defineThemeConfig({
      * 公告板
      * @see https://theme-plume.vuejs.press/guide/features/bulletin/
      */
-//     bulletin: {
-//         layout: 'bottom-right',
-//         title: '项导文档更新公告',
-//         contentType: 'markdown',
-//         content: `\
-// **2025-03-17**
-// - 添加许多项目文档
-// 加入开发组，一起完善文档！
-// `,
-//     },
+    bulletin: {
+        layout: 'top-right',
+        title: '项导文档迁移公告',
+        contentType: 'markdown',
+        content: `\
+**2025-08-09**
+- 项导文档主域名 docs.pguide.studio 即将到期，请用户切换至 docs.pguide.cloud
+`,
+    },
 
     /* 过渡动画 @see https://theme-plume.vuejs.press/config/basic/#transition */
     transition: {
